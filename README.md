fd8b3bbd395a5d01271995964d1fdf3f291209e3
Thanh Nguyen <>
2023-01-17 12:20:15 +0700

    Init project with Laravel and React

---
+++ README.md
@@ -0,0 +1,27 @@
+# Laravel React Full Stack Application
+## Full Stack Take Home Coding Exercise
+Please write an app using React JS on the front end and PHP (Laravel) on the back
+end. The app should have the following functionality:
+● User can create a log in
+● Next, user can fill out a profile and upload their picture and some basic
+personal information (name + phone number + custom-id)
+● This information is saved to the database and a profile URL is generated, e.g.
+{server}/profile/{custom-id}
+
+## Installation
+Make sure you have environment setup properly. You will need PHP8.2, composer and Node.js.
+
+### Prerequire: Docker, PHP installed
+For more information: https://laravel.com/docs/sail
+
+1. Download the project (or clone using GIT)
+2. Copy `.env.example` into `.env` and configure database credentials
+3. Navigate to the project's root directory using terminal
+4. Run `composer install`, `php artisan sail:install`
+5. Set the encryption key by executing `php artisan key:generate --ansi`
+6. Start local server by executing `./vendor/bin/sail up -d`
+7. Run migrations `./vendor/bin/sail artisan migrate`
+8. Open new terminal and navigate to the `react` folder
+9. Copy `react/.env.example` into `.env` and adjust the `VITE_API_BASE_URL` parameter
+9. Run `npm install`
+10. Run `npm run dev` to start vite server for React
---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        $customId = IdGenerator::generate(['table' => 'users', 'length' => 6, 'prefix' => date('y')]);
+
+        $data = $request->validated();
+        /** @var \App\Models\User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+            'custom_id' => $customId,
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email or password is incorrect'
+            ], 422);
+        }
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+
+    public function logout(Request $request)
+    {
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response('', 204);
+    }
+}
